544d433efb14542a447d7f5b5aef8ea5b8b255ff
louay-hesham <louay.hesham@gmail.com>
2017-08-07 20:55:55 +0200

    code restructuring

--- Main.py
+++ Main.py
@@ -4,21 +4,21 @@
 #It should give direct links to the torrents
 import time
 import requests
+import os
 from bs4 import BeautifulSoup
 
 while True:
-   time.sleep(60)
-   request=requests.get('http://eztv.ag/search/rick-and-morty')
-   if request.status_code!=200:
-       raise BaseException('connection Error, Retrying')
-       continue
-   html=request.content
-   soup = BeautifulSoup(html, 'html.parser')
-   links=[i['href'] for i in soup.find_all('a') if 'S03E02' in i['href']]
-   if links==[]:
-       print ('Not Here yet')
-   else:
-       print ('''
+    request = requests.get('http://eztv.ag/search/rick-and-morty')
+    if request.status_code != 200:
+        raise BaseException('connection Error, Retrying')
+        continue
+    html = request.content
+    soup = BeautifulSoup(html, 'html.parser')
+    links = [i['href'] for i in soup.find_all('a') if 'S03E03' in i['href']]
+    if links == []:
+        print('Not Here yet')
+    else:
+        print('''
  __       __            __                        __                  __                        _______             __              _______             __
 /  |  _  /  |          /  |                      /  |                /  |                      /       \           /  |            /       \           /  |
 $$ | / \ $$ | __    __ $$ |____    ______        $$ |       __    __ $$ |____    ______        $$$$$$$  | __    __ $$ |____        $$$$$$$  | __    __ $$ |____
@@ -32,7 +32,11 @@ $$/      $$/  $$$$$$/  $$$$$$$/   $$$$$$$/       $$$$$$$$/  $$$$$$/  $$$$$$$/   
                                                                               
 ''')
   
-       for i in links:
-           if 'http' in i:
-               print (i)
-       break
+        for i in links:
+            if 'http' in i:
+               print(i)
+            else:
+                os.startfile(i)
+        break
+    time.sleep(5)
+
