b1ce4866eadbbbdcdb33c345035fd91a20960be9
louay-hesham <louay.hesham@gmail.com>
2019-05-20 15:56:27 +0200

    Added support for other shows dynamically

--- Main.py
+++ Main.py
@@ -1,7 +1,41 @@
 from TvShow import TvShow
 
-RM = TvShow('Rick and Morty', 'http://eztv.ag/search/rick-and-morty', 3, 3)
-x = RM.get_next_episode_links()
-for i in x:
-    print(i)
+# Specify your bittorrent client
+bittorrent_client='deluge'
 
+# Specify your shows details
+# Beginning_season and beginning_episode define the exact episode that the search starts from.
+# It should download all the episodes in all seasons after that episodes.
+# If you enter 1,1 respectively, it will download the entire series.
+
+# The code opens magnet links into your favourite bittorrent client.
+# This has been tested using Deluge. Other bittorrent clients may require some modifications in the code.
+# Only works on Unix for now. Windows uses a different set of commands to start an application.
+# Support for Windows may or may not come in the future (I hate coding on Windows).
+# I recommend use eztv.io for all your torrent links, you may try other websites if they have the same general layout.
+shows = {
+    # "The Flash": {
+    #     "link": "https://eztv.io/shows/1058/the-flash-2014/",
+    #     "beginning_season": 5,
+    #     "beginning_episode": 13,
+    #     "extra": "x265"
+    # },
+    "Rick and Morty": {
+        "link": "https://eztv.io/shows/1017/rick-and-morty/"
+        "beginning_season": 4,
+        "beginning_episode": 1,
+        "extra": ""
+    },
+    "The Big Bang Theory": {
+        "link": "https://eztv.io/shows/23/the-big-bang-theory/",
+        "beginning_season": 12,
+        "beginning_episode": 13,
+        "extra": "x265"
+    },
+}
+
+for show_title, show_properties in shows.items():
+    show = TvShow(show_title, show_properties, bittorrent_client)
+    x = show.get_next_episodes_links(download=True)
+    for i in x:
+        print(i)
--- TvShow.py
+++ TvShow.py
@@ -7,17 +7,34 @@ from bs4 import BeautifulSoup
 
 
 class TvShow(object):
-    def __init__(self, name, link, season, episode):
+    def __init__(self, name, show_properties, bittorrent_client):
         self.name = name
-        self.link = link
-        self.season = season
-        self.episode = episode
+        self.link = show_properties['link']
+        self.season = show_properties['beginning_season']
+        self.episode = show_properties['beginning_episode']
+        self.extra_prop = show_properties['extra']
+        self.bittorrent_client = bittorrent_client
 
     def __get_episode_name(self):
-        return 'S' + ('0' if self.season < 10 else '') + str(self.season) + 'E' + ('0' if self.episode < 10 else '') + str(self.episode)
+        return 'E' + ('0' if self.episode < 10 else '') + str(self.episode)
 
-    def get_next_episode_links(self):
-        
+    def __get_season_name(self):
+        return 'S' + ('0' if self.season < 10 else '') + str(self.season)
+
+    def __has_episode(self, links):
+        for link in links:
+            if self.__get_episode_name() in link:
+                return True
+        return False
+
+    def __has_season(self, links):
+        for link in links:
+            if self.__get_season_name() in link:
+                return True
+        return False
+
+    def get_next_episodes_links(self, extra='', download=False):
+
         request = requests.get(self.link)
         if request.status_code != 200:
             raise BaseException('Connection Error, Retrying')
@@ -27,30 +44,42 @@ class TvShow(object):
             request = requests.get(self.link)
             if request.status_code != 200:
                 raise BaseException('Connection Error, Retrying')
-            
+
         html = request.content
         soup = BeautifulSoup(html, 'html.parser')
-        links = [i['href'] for i in soup.find_all('a') if self.__get_episode_name() in i['href']]
-        if links == []:
+        links = [i['href'] for i in soup.find_all('a')]
+
+        filtered_links = []
+        while self.__has_season(links):
+            while self.__has_episode(links):
+                filtered_links.extend([link for link in links if 'magnet' in link and self.extra in link and self.__get_season_name() in link and self.__get_episode_name() in link])
+                self.episode += 1
+            self.episode = 1
+            self.season += 1
+
+
+        if filtered_links == []:
             print('Not Here yet')
-            return
+            return []
         else:
             print('''
      __       __            __                        __                  __                        _______             __              _______             __
-    /  |  _  /  |          /  |                      /  |                /  |                      /       \           /  |            /       \           /  |
-    $$ | / \ $$ | __    __ $$ |____    ______        $$ |       __    __ $$ |____    ______        $$$$$$$  | __    __ $$ |____        $$$$$$$  | __    __ $$ |____
-    $$ |/$  \$$ |/  |  /  |$$      \  /      \       $$ |      /  |  /  |$$      \  /      \       $$ |  $$ |/  |  /  |$$      \       $$ |  $$ |/  |  /  |$$      \
+    /  |  _  /  |          /  |                      /  |                /  |                      /       \\           /  |            /       \\           /  |
+    $$ | / \\ $$ | __    __ $$ |____    ______        $$ |       __    __ $$ |____    ______        $$$$$$$  | __    __ $$ |____        $$$$$$$  | __    __ $$ |____
+    $$ |/$  \\$$ |/  |  /  |$$      \\  /      \\       $$ |      /  |  /  |$$      \\  /      \\       $$ |  $$ |/  |  /  |$$      \\       $$ |  $$ |/  |  /  |$$      \
     $$ /$$$  $$ |$$ |  $$ |$$$$$$$  | $$$$$$  |      $$ |      $$ |  $$ |$$$$$$$  | $$$$$$  |      $$ |  $$ |$$ |  $$ |$$$$$$$  |      $$ |  $$ |$$ |  $$ |$$$$$$$  |
     $$ $$/$$ $$ |$$ |  $$ |$$ |  $$ | /    $$ |      $$ |      $$ |  $$ |$$ |  $$ | /    $$ |      $$ |  $$ |$$ |  $$ |$$ |  $$ |      $$ |  $$ |$$ |  $$ |$$ |  $$ |
-    $$$$/  $$$$ |$$ \__$$ |$$ |__$$ |/$$$$$$$ |      $$ |_____ $$ \__$$ |$$ |__$$ |/$$$$$$$ |      $$ |__$$ |$$ \__$$ |$$ |__$$ |      $$ |__$$ |$$ \__$$ |$$ |__$$ |
+    $$$$/  $$$$ |$$ \\__$$ |$$ |__$$ |/$$$$$$$ |      $$ |_____ $$ \\__$$ |$$ |__$$ |/$$$$$$$ |      $$ |__$$ |$$ \\__$$ |$$ |__$$ |      $$ |__$$ |$$ \\__$$ |$$ |__$$ |
     $$$/    $$$ |$$    $$/ $$    $$/ $$    $$ |      $$       |$$    $$/ $$    $$/ $$    $$ |      $$    $$/ $$    $$/ $$    $$/       $$    $$/ $$    $$/ $$    $$/
     $$/      $$/  $$$$$$/  $$$$$$$/   $$$$$$$/       $$$$$$$$/  $$$$$$/  $$$$$$$/   $$$$$$$/       $$$$$$$/   $$$$$$/  $$$$$$$/        $$$$$$$/   $$$$$$/  $$$$$$$/
-                                                                                                                                                          
-                                                                              
+
+
     ''')
+            self.filtered_links = filtered_links
+            if download:
+                self.download_all()
+            return filtered_links
 
-            self.links = links
-            return links
-
-    def open_episode(self, i):
-        os.startfile(self.links[(i - 1) * 2])
+    def download_all(self):
+        for link in self.filtered_links:
+            os.system(self.bittorrent_client + " " + link)
